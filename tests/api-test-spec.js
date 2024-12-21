74234246e0e33f41f8839a0b2b087f8caf53b476
van bachdim <tia.annisatia@gmail.com>
2024-12-21 22:16:00 +0700

    first commit

---
+++ tests/api-test-spec.js
@@ -0,0 +1,9 @@
+const {test} = require("@playwright/test")
+
+test('Test Case 1', async({page}) => {
+    console.log("Dieksekusi dari test case 1");
+});
+
+test('Test Case 2', async({page}) => {
+    console.log("Dieksekusi dari test case 2");
+});
